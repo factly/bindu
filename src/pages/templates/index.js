e9de8b8627640df00e0305748a4cf2ad40db5f6d
vshian <vvardhanreddy4@gmail.com>
2021-03-24 22:49:04 +0530

    refactor(templates): updated templates index file to work with data from backend

--- src/pages/templates/index.js
+++ src/pages/templates/index.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { List, Card } from 'antd';
+import { List, Card, Popconfirm, Button } from 'antd';
 import { Link } from 'react-router-dom';
+import { EditOutlined, DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+
+import { deleteTemplate, getTemplates } from '../../actions/templates';
 
 const { Meta } = Card;
+
 function Templates() {
-  const templates = useSelector((state) => state.templates);
-  const { options } = templates;
+  const dispatch = useDispatch();
+  const templates = useSelector((state) => Object.values(state.templates.details));
+
+  useEffect(() => {
+    dispatch(getTemplates());
+  }, []);
+
+  const handleDelete = (id) => {
+    dispatch(deleteTemplate(id));
+  };
+
   return (
     <List
+      header={
+        <Link to={'/templates/create'}>
+          <Button type="primary" icon={<PlusOutlined />} size={'large'}>
+            Add new
+          </Button>
+        </Link>
+      }
       grid={{ gutter: 16, column: 5 }}
-      dataSource={options}
-      renderItem={(d, i) => (
+      dataSource={templates}
+      renderItem={(template) => (
         <List.Item>
-          <Link key={i} to={'/chart/' + i}>
-            <Card hoverable cover={<img alt="example" src={d.icon} />}>
-              <Meta title={d.name} />
-            </Card>
-          </Link>
+          <Card
+            hoverable
+            cover={<img alt="example" src={template.icon} />}
+            actions={[
+              <Link to={'/templates/' + template.id + '/edit'}>
+                <EditOutlined key="edit" />
+              </Link>,
+              <Popconfirm
+                title="Are you sure to delete this template?"
+                onConfirm={() => handleDelete(template.id)}
+                okText="Yes"
+                cancelText="No"
+              >
+                <DeleteOutlined key="ellipsis" />
+              </Popconfirm>,
+            ]}
+          >
+            <Link to={'/chart/' + template.id}>
+              <Meta title={template.title} />
+            </Link>
+          </Card>
         </List.Item>
       )}
     />
