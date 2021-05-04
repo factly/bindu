cf5586dfaf6000264255e62da811275455a3953a
hetjagani <hetpatel572@gmail.com>
2021-05-04 08:59:38 -0700

    Changed server calls to DB queries in bindu migrate scripts. Implemented logic: If found in DB then update the templates else create.

--- cmd/migrate-templates.go
+++ cmd/migrate-templates.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
@@ -15,19 +14,15 @@ import (
 	"github.com/factly/bindu-server/model"
 	minioutil "github.com/factly/bindu-server/util/minio"
 	"github.com/factly/x/middlewarex"
-	"github.com/factly/x/requestx"
+	"github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/minio/minio-go/v7"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var (
-	TemplatesPath string            = "./templates"
-	headers       map[string]string = map[string]string{
-		"X-Space": "0",
-		"X-User":  "0",
-	}
-	BinduURL string = "http://localhost:8000"
+	TemplatesPath string = "./templates"
+	SpaceID       uint   = 0
 )
 
 func init() {
@@ -40,26 +35,16 @@ func init() {
 
 	TemplatesPath = "./templates"
 
-	BinduURL = viper.GetString("bindu_url")
 }
 
 var migrateTemplatesCmd = &cobra.Command{
 	Use:   "migrate-templates",
 	Short: "Apply migrations for templates data for bindu-server.",
 	Run: func(cmd *cobra.Command, args []string) {
-		spaceID, err := GetSuperOrgSpace()
+		var err error
+		SpaceID, err = GetSuperOrgSpace()
 		if err != nil {
 			log.Fatal(err.Error())
-		}
-
-		userID, err := GetUserID()
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		headers = map[string]string{
-			"X-Space": fmt.Sprint(spaceID),
-			"X-User":  userID,
 		}
 
 		err = MigrateTemplate()
@@ -67,35 +52,6 @@ var migrateTemplatesCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 	},
-}
-
-func GetUserID() (string, error) {
-	kavachUserCheckers := map[string]interface{}{
-		"extra": map[string]interface{}{
-			"identity": map[string]interface{}{
-				"traits": map[string]interface{}{
-					"email": viper.GetString("default_user_email"),
-				},
-			},
-		},
-	}
-
-	resp, err := requestx.Request("POST", viper.GetString("kavach_url")+"/users/checker", kavachUserCheckers, map[string]string{
-		"Content-Type": "application/json",
-	})
-	if err != nil {
-		return "", err
-	}
-
-	var respBody map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&respBody)
-	if err != nil {
-		return "", err
-	}
-
-	headerMap := respBody["header"].(map[string]interface{})
-	userIDArr := headerMap["X-User"].([]interface{})
-	return userIDArr[0].(string), nil
 }
 
 func GetSuperOrgSpace() (uint, error) {
@@ -134,104 +90,109 @@ func MigrateTemplate() error {
 		categories_paths = append(categories_paths, fmt.Sprint(TemplatesPath, "/", each.Name()))
 	}
 
-	_, migrated := CategoriesMigrated(categories)
+	category_map := make(map[string]uint)
+	presentCategories := make([]model.Category, 0)
+	// fetch all categories
+	config.DB.Model(&model.Category{}).Find(&presentCategories)
 
-	if !migrated {
-		category_map := make(map[string]uint)
-		for _, category_name := range categories {
+	for _, cat := range presentCategories {
+		category_map[cat.Name] = cat.ID
+	}
+
+	// Adds categories if any new found
+	for _, category_name := range categories {
+		if _, found := category_map[category_name]; !found {
 			category := model.Category{
-				Name: category_name,
+				Name:    category_name,
+				SpaceID: SpaceID,
 			}
 
-			resp, err := requestx.Request("POST", BinduURL+"/categories", category, headers)
-			if err != nil {
+			if err = config.DB.Create(&category).Error; err != nil {
 				return err
 			}
+			category_map[category.Name] = category.ID
+		}
+	}
 
-			if resp.StatusCode != http.StatusCreated {
-				return errors.New("could not create category " + category_name)
-			}
-			defer resp.Body.Close()
-
-			respCategory := model.Category{}
-			if err := json.NewDecoder(resp.Body).Decode(&respCategory); err != nil {
-				return err
-			}
-
-			category_map[respCategory.Name] = respCategory.ID
+	for _, cat_path := range categories_paths {
+		files, err := ioutil.ReadDir(cat_path)
+		if err != nil {
+			return err
 		}
 
-		for _, cat_path := range categories_paths {
-			files, err := ioutil.ReadDir(cat_path)
+		fmt.Println("Processing files in " + cat_path)
+
+		for _, file := range files {
+			filepath := fmt.Sprint(cat_path, "/", file.Name())
+			category_name := strings.Split(cat_path, "/")[2]
+			chart_name := file.Name()
+			fmt.Println("Processing ", filepath)
+
+			// fetching properties
+			propertiesFile, err := os.Open(fmt.Sprint(filepath, "/properties.json"))
+			if err != nil {
+				return err
+			}
+			defer propertiesFile.Close()
+
+			propertiesBytes, err := ioutil.ReadAll(propertiesFile)
 			if err != nil {
 				return err
 			}
 
-			fmt.Println("Processing files in " + cat_path)
+			// fetching spec
+			specFile, err := os.Open(fmt.Sprint(filepath, "/spec.json"))
+			if err != nil {
+				return err
+			}
+			defer specFile.Close()
 
-			for _, file := range files {
-				filepath := fmt.Sprint(cat_path, "/", file.Name())
-				category_name := strings.Split(cat_path, "/")[2]
-				chart_name := file.Name()
-				fmt.Println("Processing ", filepath)
+			specBytes, err := ioutil.ReadAll(specFile)
+			if err != nil {
+				return err
+			}
 
-				// fetching properties
-				var properties []map[string]interface{}
-				propertiesFile, err := os.Open(fmt.Sprint(filepath, "/properties.json"))
-				if err != nil {
+			mediumID, err := CreateMedium(filepath, fmt.Sprint(chart_name, ".png"), "thumbnail.png")
+			if err != nil {
+				return err
+			}
+
+			template := model.Template{
+				CategoryID: category_map[category_name],
+				MediumID:   &mediumID,
+				Properties: postgres.Jsonb{
+					RawMessage: propertiesBytes,
+				},
+				Spec: postgres.Jsonb{
+					RawMessage: specBytes,
+				},
+				Title:   chart_name,
+				Slug:    strings.ToLower(chart_name),
+				SpaceID: SpaceID,
+			}
+
+			presentTemplate := model.Template{
+				Title:   chart_name,
+				SpaceID: SpaceID,
+			}
+
+			err = config.DB.Model(&model.Template{}).Where(&presentTemplate).First(&presentTemplate).Error
+			if err != nil {
+				// not found any such template
+				if err = config.DB.Create(&template).Error; err != nil {
 					return err
-				}
-				defer propertiesFile.Close()
-
-				bytes, _ := ioutil.ReadAll(propertiesFile)
-				err = json.Unmarshal(bytes, &properties)
-				if err != nil {
-					return err
-				}
-
-				// fetching spec
-				var spec map[string]interface{}
-				specFile, err := os.Open(fmt.Sprint(filepath, "/spec.json"))
-				if err != nil {
-					return err
-				}
-				defer specFile.Close()
-
-				bytes, _ = ioutil.ReadAll(specFile)
-				err = json.Unmarshal(bytes, &spec)
-				if err != nil {
-					return err
-				}
-
-				mediumID, err := CreateMedium(filepath, fmt.Sprint(chart_name, ".png"), "thumbnail.png")
-				if err != nil {
-					return err
-				}
-				fmt.Println(`created medium`, chart_name)
-
-				templateBody := map[string]interface{}{
-					"category_id": category_map[category_name],
-					"medium_id":   mediumID,
-					"properties":  properties,
-					"spec":        spec,
-					"title":       chart_name,
-					"slug":        strings.ToLower(chart_name),
-				}
-
-				resp, err := requestx.Request("POST", BinduURL+"/templates", templateBody, headers)
-				if err != nil {
-					return err
-				}
-
-				if resp.StatusCode != http.StatusCreated {
-					return errors.New(`cannot create template ` + chart_name)
 				} else {
 					fmt.Println("template " + chart_name + " created")
 				}
+			} else {
+				// found template
+				if err = config.DB.Model(&presentTemplate).Updates(template).Error; err != nil {
+					return err
+				} else {
+					fmt.Println("template " + chart_name + " updated")
+				}
 			}
 		}
-	} else {
-		fmt.Println("Templates migrations done...")
 	}
 
 	return nil
@@ -243,59 +204,28 @@ func CreateMedium(path, chartName, filename string) (uint, error) {
 		return 0, err
 	}
 
-	mediumBody := map[string]interface{}{
-		"name": chartName,
-		"url": map[string]interface{}{
-			"raw": fmt.Sprint("http://", viper.GetString("minio_public_url"), "/", viper.GetString("minio_bucket"), "/bindu/", chartName),
+	urlBytes, _ := json.Marshal(map[string]interface{}{
+		"raw": fmt.Sprint("http://", viper.GetString("minio_public_url"), "/", viper.GetString("minio_bucket"), "/bindu/", chartName),
+	})
+
+	medium := model.Medium{
+		Name: chartName,
+		URL: postgres.Jsonb{
+			RawMessage: urlBytes,
 		},
-		"file_size": info.Size,
+		FileSize: info.Size,
+		SpaceID:  SpaceID,
 	}
 
-	resp, err := requestx.Request("POST", BinduURL+"/media", mediumBody, headers)
-	if err != nil {
-		return 0, err
-	}
-
-	var respMedium model.Medium
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&respMedium)
-	if err != nil {
-		return 0, err
-	}
-
-	return respMedium.ID, nil
-}
-
-func CategoriesMigrated(categoryNames []string) (map[string]uint, bool) {
-
-	resp, err := requestx.Request("GET", BinduURL+"/categories", nil, headers)
-	if err != nil {
-		return nil, false
-	}
-
-	type catPage struct {
-		Nodes []model.Category `json:"nodes,omitempty"`
-		Total int              `json:"total,omitempty"`
-	}
-
-	categoriesPaiganation := catPage{}
-	defer resp.Body.Close()
-
-	if err := json.NewDecoder(resp.Body).Decode(&categoriesPaiganation); err != nil {
-		return nil, false
-	}
-
-	categories := categoriesPaiganation.Nodes
-	categoryMap := make(map[string]uint)
-	for _, cat := range categories {
-		categoryMap[cat.Name] = cat.ID
-	}
-
-	for _, category := range categoryNames {
-		if _, found := categoryMap[category]; !found {
-			return nil, false
+	if err = config.DB.Model(&model.Medium{}).Where(&medium).First(&medium).Error; err == nil {
+		return medium.ID, nil
+	} else {
+		// create medium
+		if err = config.DB.Create(&medium).Error; err != nil {
+			return medium.ID, err
 		}
+		fmt.Println("created medium ", chartName)
 	}
-	return categoryMap, true
+
+	return medium.ID, nil
 }
