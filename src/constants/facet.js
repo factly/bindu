d2cf08a9178c8a2358db1cc1505319daa34a2ed2
vshian <vvardhanreddy4@gmail.com>
2021-05-08 14:11:59 +0530

    removed unused files

--- src/constants/facet.js
+++
@@ -1,4 +0,0 @@
-export const SET_FACET_COLUMNS = 'set-facet-columns';
-export const SET_FACET_SPACING = 'set-facet-spacing';
-export const SET_FACET_XAXIS = 'set-facet-xaxis';
-export const SET_FACET_YAXIS = 'set-facet-yaxis';
